e3c886007bb63a0464e35e689cf438cac65f4489
Niels Groen <hooded.main@gmail.com>
2025-02-21 22:08:27 +0100

    Try out some tailwind on the lucia demo page

--- src/routes/demo/lucia/+page.svelte
+++ src/routes/demo/lucia/+page.svelte
@@ -5,8 +5,10 @@
 	let { data }: { data: PageServerData } = $props();
 </script>
 
-<h1>Hi, {data.user.username}!</h1>
-<p>Your user ID is {data.user.id}.</p>
-<form method='post' action='?/logout' use:enhance>
-	<button>Sign out</button>
-</form>
+<div class="h-screen flex justify-center items-center text-center"><div class="bg-cyan-300 rounded-lg p-4">
+	<h1>Hi, {data.user.username}!</h1>
+	<p>Your user ID is {data.user.id}.</p>
+	<form method='post' action='?/logout' use:enhance>
+		<button class="border border-solid border-black rounded-md p-1">Sign out</button>
+	</form>
+</div></div>
--- src/routes/demo/lucia/login/+page.svelte
+++ src/routes/demo/lucia/login/+page.svelte
@@ -5,17 +5,19 @@
 	let { form }: { form: ActionData } = $props();
 </script>
 
-<h1>Login/Register</h1>
-<form method='post' action='?/login' use:enhance>
-	<label>
-		Username
-		<input name='username' />
-	</label>
-	<label>
-		Password
-		<input type='password' name='password' />
-	</label>
-	<button>Login</button>
-	<button formaction='?/register'>Register</button>
-</form>
-<p style='color: red'>{form?.message ?? ''}</p>
+<div class="h-screen flex justify-center items-center text-center"><div class="bg-cyan-300 rounded-lg p-4">
+	<h1 class="font-bold text-2xl mb-3">Login/Register</h1>
+	<form method='post' action='?/login' use:enhance>
+		<label>
+			Username <br>
+			<input class="border border-solid border-cyan-650 rounded-md bg-cyan-200" name='username' />
+		</label><br>
+		<label>
+			Password <br>
+			<input class="border border-solid border-black rounded-md bg-cyan-200" type='password' name='password' />
+		</label><br>
+		<button class="mt-2 border border-solid border-black rounded-md p-1">Login</button>
+		<button class="border border-solid border-black rounded-md p-1" formaction='?/register'>Register</button>
+	</form>
+	<p style='color: red'>{form?.message ?? ''}</p>
+</div></div>
